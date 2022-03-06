f4fa80e8b898dd000c3178f9ce4127f72c93869e
skpig <479469418@qq.com>
2022-03-06 19:56:01 +0800

    1. Add class UserExtension to extend default User.(models.py) 2. Add IndexView and LoginView (view.py) 3. Use LoginRequiredMixin to enforce login (views.py)

--- dbmid/app/models.py
+++ dbmid/app/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+# Create our own DBUser based on django.contrib.auth.models.User
+# According to https://docs.djangoproject.com/zh-hans/4.0/topics/auth/customizing/#extending-the-existing-user-models
+class UserExtension(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='extension')
+    # Add new user field Here
+    # ...
+
+
+@receiver(post_save, sender=User)
+def create_user_extension(sender, instance, created, **kwargs):
+    if created:
+        UserExtension.objects.create(user=instance)
+    else:
+        instance.extension.save()
--- dbmid/app/views.py
+++ dbmid/app/views.py
@@ -1,3 +1,36 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import TemplateView
 
-# Create your views here.
+# Base class for all views (except for loginView) to enforce login
+class MyView(LoginRequiredMixin, TemplateView):
+    login_url = 'login/'
+
+
+class IndexView(MyView):
+    template_name = 'app/index.html'
+    def post(self, request, *args, **kwargs):
+        if 'logout' in request.POST:
+            logout(request)
+            return HttpResponseRedirect(reverse('app:login_view'))
+
+
+class LoginView(TemplateView):
+    template_name = 'app/login.html'
+
+    def post(self, request, *args, **kwargs):
+        print(*args, **kwargs)
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username, password)
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('app:index_view'))
+        else:
+            return HttpResponse("Wrong username / password.")
+
